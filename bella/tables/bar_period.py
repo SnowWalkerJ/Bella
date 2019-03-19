314383e0ba99457f1e7074bc82d1c07ca2332335
SnowWalkerJ <jike3212001@163.com>
2019-03-19 17:12:10 +0800

    remove mongoengine tables

--- bella/tables/bar_period.py
+++
@@ -1,5 +0,0 @@
-from mongoengine import Document, StringField
-
-
-class BarPeriod(Document):
-    period = StringField(regex="[0-9]{1,2}[smhd]")
--- bella/tables/scheduler.py
+++
@@ -1,9 +0,0 @@
-from mongoengine import Document, StringField, BooleanField
-
-
-class Task(Document):
-    Name      = StringField(primary_key=True)
-    Command   = StringField()
-    LogFile   = StringField()
-    Crontab   = StringField()
-    Activated = BooleanField()
