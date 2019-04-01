0af255f8765d0962d0b63ef0a2ea2a5b41c1dddb
SnowWalkerJ <jike3212001@163.com>
2019-04-01 09:33:46 +0800

    csrf

--- bella_rest/bella_rest/settings.py
+++ bella_rest/bella_rest/settings.py
@@ -45,12 +45,15 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'bella_rest.urls'
 
